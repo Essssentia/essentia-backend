0d4dfd745ec39136d162727c3b7e12a1c8349339
gminnimk <tvt1245@naver.com>
2024-11-27 15:52:00 +0900

    init commit

---
+++ src/main/java/com/sw/essentiabackend/EssentiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sw.essentiabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EssentiaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sw/essentiabackend/EssentiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.essentiabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentiaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
