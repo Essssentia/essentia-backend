88ab4870fffc58e88361dcacf557b1587ae13aa3
gminnimk <165118770+gminnimk@users.noreply.github.com>
2024-11-29 16:34:15 +0900

    fix : Spring 버전 바뀜에 따라 RestTemplateConfig 로직 수정

--- src/main/java/com/sw/essentiabackend/config/RestTemplateConfig.java
+++ src/main/java/com/sw/essentiabackend/config/RestTemplateConfig.java
@@ -1,36 +1,29 @@
 package com.sw.essentiabackend.config;
 
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 /**
- * 외부 API 통신을 위한 RestTemplate 설정 파일로, 타임아웃 설정을 포함합니다.
+ * 외부 API와의 통신을 위해 사용되는 RestTemplate의 타임아웃 설정을 정의하고, 애플리케이션 내에서 사용할 수 있도록 빈으로 등록
  */
 @Configuration
 public class RestTemplateConfig {
 
+    /**
+     * RestTemplate 빈을 생성하고 설정
+     *
+     * @param restTemplateBuilder RestTemplate을 빌드하기 위한 빌더 객체입니다.
+     * @return RestTemplate 외부 API 호출을 위한 RestTemplate 객체입니다.
+     */
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        // HTTP 클라이언트 생성
-        CloseableHttpClient httpClient = HttpClients.custom()
-            .setConnectionManager(new PoolingHttpClientConnectionManager())
-            .build();
-
-        // 타임아웃 설정
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
-            httpClient);
-        requestFactory.setConnectTimeout(5000); // 연결 시간 초과 (ms)
-        requestFactory.setReadTimeout(5000);   // 읽기 시간 초과 (ms)
-
-        // RestTemplate 빌드
         return restTemplateBuilder
-            .requestFactory(() -> requestFactory)
+            .setConnectTimeout(Duration.ofSeconds(5))
+            .setReadTimeout(Duration.ofSeconds(5))
             .build();
     }
 }
